55f139eccada24ff802b760dff33f4b1ca73756d
heerey525 <190182347@qq.com>
2020-09-07 14:00:46 +0800

    重新打包生成预览

---
+++ dist/precache-manifest.f873dfb82ae413c63216942ec6cd8b43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49a3baea35b1fac8a69a4fec719a010d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3df70343a118551e8a6",
+    "url": "/static/css/2.48db8b0f.chunk.css"
+  },
+  {
+    "revision": "f3df70343a118551e8a6",
+    "url": "/static/js/2.a8eb6590.chunk.js"
+  },
+  {
+    "revision": "f944d98719c43c60a7bd",
+    "url": "/static/js/main.580f879e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.98646569a3cbf3207632dbbd08426500.js"
+  "/precache-manifest.f873dfb82ae413c63216942ec6cd8b43.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
